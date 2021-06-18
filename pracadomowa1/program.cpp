6dcff48bfab3bb878a33c19e55aa287e253a2f85
Aleksander Siudek <alek@siudek.net>
2021-06-18 18:55:08 +0200

    IMP

--- pracadomowa1/program.cpp
+++ pracadomowa1/program.cpp
@@ -14,6 +14,46 @@ struct DaneOsobowe
   char plec;
 };
 
+DaneOsobowe dane[100];
+int indexOfPerson = 0;
+
+void printData()
+{
+  cout << endl;
+  for (int i = 0; i < indexOfPerson; i++)
+  {
+    cout << dane[i].imie << " " << dane[i].nazwisko << " " << dane[i].wiek << " " << dane[i].plec << endl;
+  }
+  cout << endl;
+}
+
+void addNew()
+{
+  cout << "podaj dane pacjenta: " << endl;
+
+  cout << "imie: " << endl;
+  string firstName;
+  cin >> firstName;
+
+  cout << "nazwisko: " << endl;
+  string surname;
+  cin >> surname;
+
+  cout << "wiek: " << endl;
+  int age;
+  cin >> age;
+
+  cout << "plec: " << endl;
+  char gender;
+  cin >> gender;
+
+  dane[indexOfPerson].imie = firstName;
+  dane[indexOfPerson].nazwisko = surname;
+  dane[indexOfPerson].wiek = age;
+  dane[indexOfPerson].plec = gender;
+  indexOfPerson++;
+}
+
 int main(void)
 {
 
@@ -23,11 +63,9 @@ int main(void)
   // fstream resultFile;
   // resultFile.open(".txt",ios::out);
 
-  DaneOsobowe dane[100];
-
   bool firstLine = true;
   string line;
-  int indexOfPerson = 0;
+
   while (getline(dataFile, line))
   {
     if (firstLine)
@@ -62,19 +100,25 @@ int main(void)
   while (true)
   {
     cout
+        << endl
         << "1.Wyświetl dane" << endl
         << "2.Ilość pacjentów w bazie " << endl
         << "3.dodaj nowego" << endl
-        << "9.zakoncz program" << endl;
+        << "9.zakoncz program" << endl
+        << endl;
     int decision;
     cin >> decision;
 
     switch (decision)
     {
     case 1:
+      printData();
       break;
     case 2:
       cout << "Ilość pacjentów w bazie wynosi :" << indexOfPerson << endl;
+      break;
+    case 3:
+      addNew();
       break;
     case 9:
       return 0;
@@ -82,19 +126,6 @@ int main(void)
       break;
     }
   }
-
-  // dane[0].imie = "Jan";
-  // dane[0].nazwisko= "Kowalski";
-  // dane[0].plec = 'M';
-  // dane[0].wiek = 45;
-
-  // dane[1].imie = "Adam";
-  // dane[1].nazwisko= "Nowak";
-  // dane[1].plec = 'M';
-  // dane[1].wiek = 65;
-
-  // cout << dane[0].imie << dane[0].nazwisko << dane[0].wiek << dane[0].plec << endl;
-  // cout << dane[1].imie << dane[1].nazwisko << dane[1].wiek << dane[1].plec << endl;
 
   cout << endl
        << endl;
