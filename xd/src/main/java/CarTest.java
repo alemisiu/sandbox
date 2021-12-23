7fb6ddac77d55d5aa84f556a118ecab8128159d2
aleksander.siudek <alek@siudek.net>
2021-12-23 14:50:52 +0100

    learning

---
+++ xd/src/main/java/CarTest.java
@@ -0,0 +1,11 @@
+public class CarTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+        cart.addProduct(new Product("mleko",1));
+        cart.addProduct(new Product("czekolada",2));
+        cart.addProduct(new Product("jogurt",3));
+
+        double cartCost = cart.totalCost();
+        System.out.println("Koszt koszyka: " + cartCost +"zł");
+    }
+}
