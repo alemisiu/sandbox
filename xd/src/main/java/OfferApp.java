a3bce9167a5b62bd44c95df8de10451a7cbf2107
aleksander.siudek <alek@siudek.net>
2021-12-29 13:41:00 +0100

    learning

---
+++ xd/src/main/java/OfferApp.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+public class OfferApp {
+    public static void main(String[] args) {
+        try{
+            Offer[ ] offers = OfferReader.readOffers("offers,csv");
+            Arrays.sort(offers);
+            for(Offer offer: offers) {
+                System.out.println(offer);
+            }
+        }catch (FileNotFoundException e) {
+            System.out.println("Brak pliku offers.csv");
+        }
+    }
+}
